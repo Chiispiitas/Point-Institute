32666e139ecb79396288e00a41cad3b2e96ed82f
Chiispiitas <davidalej1202@gmail.com>
2021-09-06 12:20:25 -0500

    Improving Number Parser

--- number-parser/js/main.js
+++ number-parser/js/main.js
@@ -12,18 +12,18 @@ const textResult = document.getElementById("text-result");
 ============================================== */
 inputNumber.addEventListener("keypress", function(event) {
      filterInput();
-     updateInputSize();
 });
 inputNumber.addEventListener("keyup", function(event) {
      filterInput();
      updateResult();
+     inputNumber.value = separateWithCommas(inputNumber.value);
 });
 /* ==============================================
      Update Result
 ============================================== */
 function updateResult() {
      if (inputNumber.value != "") {
-          textResult.textContent = capitalize(toWords(parseInt(inputNumber.value.replace(/\.|,/g, ""))));
+          textResult.textContent = capitalize(toWords(parseInt(inputNumber.value)));
      }
      else {
           textResult.textContent = "";
@@ -33,8 +33,8 @@ function updateResult() {
      Filter Input
 ============================================== */
 function filterInput() {
-     let filtered = inputNumber.value.replace(/[^\d\.,]| /g, "");
-     if (filtered.replace(/\.|,/g, "").length >= 15) {
+     let filtered = inputNumber.value.replace(/[^\d]| /g, "");
+     if (filtered.length >= 16) {
           inputNumber.value = filtered.substring(0, filtered.length - 1);
      }
      else {
@@ -48,3 +48,9 @@ function capitalize(string) {
      let lower = string.toLowerCase();
      return string.charAt(0).toUpperCase() + lower.slice(1);
 }
+/* ==============================================
+     Separate With Commas
+============================================== */
+function separateWithCommas(numberStr) {
+     return numberStr.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+ }
